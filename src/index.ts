19211eb9e620ee6453921b5751dcce2033f96cb8
minagishl <mvxd568.co@icloud.com>
2024-09-23 13:17:45 +0900

    Support for Google Site

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,11 @@ function toViewUrl(url: string): string {
     return generateViewUrl(id);
   }
 
-  return url;
+  const newURL = new URL(url);
+  newURL.searchParams.delete('authuser');
+  newURL.searchParams.delete('pli');
+
+  return newURL.toString();
 }
 
 function generateViewUrl(id: string): string {
